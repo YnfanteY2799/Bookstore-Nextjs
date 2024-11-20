762cc44684d67a6e5782c98e2c6e31e651095915
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2024-11-20 16:47:32 -0400

    +adds

---
+++ src/components/Providers/ResizableDiv.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { useMeasure } from "@/utils/client";
+import { motion } from "framer-motion";
+
+import type { IBaseComponent } from "@/types/client";
+import type { ReactNode } from "react";
+
+export default function ResiableDiv({ children, className }: IBaseComponent): ReactNode {
+  // Hooks
+  const [ref, { height }] = useMeasure<HTMLDivElement>();
+
+  return (
+    <motion.div animate={{ height }} className={className}>
+      <div ref={ref}>{children}</div>
+    </motion.div>
+  );
+}
