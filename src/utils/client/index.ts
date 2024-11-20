fd9310980036b427a470d1b4b36bc7153812a4a7
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2024-11-20 16:57:28 -0400

    +adds

--- src/utils/client/index.ts
+++ src/utils/client/index.ts
@@ -1,3 +1,7 @@
+/* Bulk Exports */
+export * from "./functions.ts";
+/* Bulk Exports */
+
 /* Hooks */
 export { default as useIsoLayout } from "./hooks/useIsoLayout.ts";
 export { default as useMeasure } from "./hooks/useMeasure.ts";
