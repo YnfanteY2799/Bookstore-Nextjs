23d3e0862b157649295b47d5980384449307de8a
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2024-11-21 15:52:15 -0400

    +adds

--- src/components/Modals/AddBook.tsx
+++ src/components/Modals/AddBook.tsx
@@ -1,5 +1,19 @@
+"use client";
+import { Button, Modal, ModalContent, useDisclosure } from "@nextui-org/react";
+import { Plus } from "@phosphor-icons/react";
 import { type ReactNode } from "react";
 
 export default function AddBookModal(): ReactNode {
-  return <div>ComponentName</div>;
+  const { isOpen, onClose, onOpenChange } = useDisclosure();
+
+  return (
+    <>
+      <Button color="primary" variant="light" isIconOnly size="sm" onPress={onOpenChange}>
+        <Plus size={20} />
+      </Button>
+      <Modal hideCloseButton backdrop="blur" size="md" placement="auto" isOpen={isOpen} onOpenChange={onClose}>
+        <ModalContent>{(onClose) => <></>}</ModalContent>
+      </Modal>
+    </>
+  );
 }
--- src/components/Modals/Login.tsx
+++ src/components/Modals/Login.tsx
@@ -2,6 +2,7 @@
 import { type ReactNode, type KeyboardEvent, useState, useRef, useEffect } from "react";
 import { type SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { ArrowLeft, User, X } from "@phosphor-icons/react";
 import { ResizableDiv } from "@/components";
 import { toast } from "sonner";
 import {
@@ -17,11 +18,119 @@ import {
 } from "@nextui-org/react";
 
 export default function LoginModal(): ReactNode {
-  const { isOpen, onClose } = useDisclosure();
+  // Hooks
+  const { isOpen, onClose, onOpenChange } = useDisclosure();
+
+  // State
+  const [resetPassword, setResetPassword] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  // Ref's
+  const formRef = useRef<HTMLFormElement>(null);
+
+  // RHF
+  const { register, reset, formState, handleSubmit, getValues } = useForm<TLoginFS>({
+    defaultValues: { email: "", password: "" },
+    resolver: zodResolver(LoginFormSchema),
+  });
+
+  const {
+    handleSubmit: handleRestore,
+    formState: restoreformState,
+    register: restoreRegister,
+    reset: restoreReset,
+    setValue,
+  } = useForm<TRecoverFS>({ resolver: zodResolver(RecoverFormSchema), defaultValues: { email: "" } });
+
+  // Destructuring Errors
+  const { email, password } = formState.errors;
+  const { email: rEmail } = restoreformState.errors;
+
+  // Functions
+  function handleSyntheticSubmit(): void {
+    const curr = formRef.current;
+    if (curr) curr.dispatchEvent(new Event("submit", { bubbles: true, cancelable: true }));
+  }
+
+  function enterSubmit({ key }: KeyboardEvent): void {
+    if (key === "Enter") handleSyntheticSubmit();
+  }
+
+  function goRestore(): void {
+    setResetPassword(() => {
+      setValue("email", getValues("email"));
+      return true;
+    });
+  }
+
+  // Form handle
+  const onSubmit: SubmitHandler<TLoginFS> = async (data) => {
+    setIsLoading(true);
+    toast.promise(serviceBasedLogin(data), {
+      loading: commons("Status.loading"),
+      error: (response: Error) => {
+        console.log("blown up", { response });
+        return response.message;
+      },
+      success: (response) => {
+        setUser && setUser(response);
+        onClose();
+        return commons("Status.welcome", { username: response.username });
+      },
+    });
+    setIsLoading(false);
+  };
+
+  const onRSubmit: SubmitHandler<TRecoverFS> = async (data) => {
+    setIsLoading(true);
+    toast.promise(serviceBasedRecover(data), {
+      loading: commons("Status.loading"),
+      error: (response: Error) => {
+        setIsLoading(false);
+        console.log({ response });
+        return response.message;
+      },
+      success: () => {
+        setIsLoading(false);
+        return "Ok";
+      },
+    });
+  };
 
   return (
-    <Modal hideCloseButton backdrop="blur" size="md" placement="auto" isOpen={isOpen} onOpenChange={onClose}>
-      <ModalContent>{(onClose) => <></>}</ModalContent>
-    </Modal>
+    <>
+      <Button color="primary" variant="light" isIconOnly size="sm">
+        <User size={20} />
+      </Button>
+      <Modal hideCloseButton backdrop="blur" size="md" placement="auto" isOpen={isOpen} onOpenChange={onClose}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex justify-between gap-2">
+                {resetPassword ? (
+                  <>
+                    <Button variant="light" onPress={goBack} isIconOnly size="sm" color="primary">
+                      <ArrowLeft size={20} />
+                    </Button>
+                    Recover your account
+                    <Button variant="light" onPress={onClose} isIconOnly size="sm" color="danger">
+                      <X size={20} />
+                    </Button>
+                  </>
+                ) : (
+                  <>
+                    <div></div>
+                    Welcome To minimal Bookstore
+                    <Button variant="light" onPress={onClose} isIconOnly size="sm" color="danger">
+                      <X size={20} />
+                    </Button>
+                  </>
+                )}
+              </ModalHeader>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
