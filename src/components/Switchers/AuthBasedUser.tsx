f596566debef6a4c8aa59ac943aef6e635ffcc2e
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2024-11-21 16:46:12 -0400

    +adds

--- src/components/Switchers/AuthBasedUser.tsx
+++ src/components/Switchers/AuthBasedUser.tsx
@@ -1,5 +1,46 @@
+"use client";
+import { Dropdown, DropdownTrigger, User } from "@nextui-org/react";
+import { User as UserIcon } from "@phosphor-icons/react";
 import { type ReactNode } from "react";
 
 export default function AuthBasedUser(): ReactNode {
-  return <></>;
+  const isLogged = false;
+
+  return (
+    <>
+      {isLogged ? (
+        <Dropdown>
+          <DropdownTrigger>
+            <User
+              name="Jane Doe"
+              description="User"
+              avatarProps={{ src: "https://i.pravatar.cc/150?u=a04258114e29026702d" }}
+            />
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Static Actions">
+        <DropdownItem key="new">New file</DropdownItem>
+        <DropdownItem key="copy">Copy link</DropdownItem>
+        <DropdownItem key="edit">Edit file</DropdownItem>
+        <DropdownItem key="delete" className="text-danger" color="danger">
+          Delete file
+        </DropdownItem>
+      </DropdownMenu>
+        </Dropdown>
+      ) : (
+        <Dropdown>
+          <DropdownTrigger>
+            <User name="Jane Doe" description="Product Designer" avatarProps={{ icon: <UserIcon /> }} />
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Static Actions">
+        <DropdownItem key="new">New file</DropdownItem>
+        <DropdownItem key="copy">Copy link</DropdownItem>
+        <DropdownItem key="edit">Edit file</DropdownItem>
+        <DropdownItem key="delete" className="text-danger" color="danger">
+          Delete file
+        </DropdownItem>
+      </DropdownMenu>
+        </Dropdown>
+      )}
+    </>
+  );
 }
