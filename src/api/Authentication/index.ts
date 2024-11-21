13224cdb3c0807550f83dc9cf20da11173cdb5b9
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2024-11-21 19:32:55 -0400

    +adds

--- src/api/Authentication/index.ts
+++ src/api/Authentication/index.ts
@@ -1,9 +1,16 @@
 "use server";
+import { createUser } from "@/utils/server";
 
-import type { TLoginFS, TRecoverFS } from "@/configs";
+import type { TLoginFS, TRecoverFS, TypeRegisterMFS } from "@/configs";
 
 export async function LoginService(data: TLoginFS): Promise<any> {}
 
 export async function RestoreService(data: TRecoverFS): Promise<any> {}
 
-export async function RegisterService() {}
+export async function RegisterService(data: TypeRegisterMFS): Promise<any> {
+  try {
+    const user = await createUser(data);
+
+    console.log({ user });
+  } catch (e) {}
+}
