d6e79c1295bdd764e3dcde87df7802ef2093aec0
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2024-11-21 15:44:47 -0400

    +adds

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Button, Popover, PopoverContent, PopoverTrigger } from "@nextui-org/react";
-import { Filter } from "lucide-react";
+import { Funnel } from "@phosphor-icons/react";
 
 import type { ReactNode } from "react";
 
@@ -9,7 +9,7 @@ export default function Filters({ onFilter }: { onFilter?: Function }): ReactNod
     <Popover placement="bottom" showArrow offset={10} backdrop="opaque">
       <PopoverTrigger>
         <Button color="primary" variant="light" isIconOnly size="sm">
-          <Filter size={20} />
+          <Funnel size={20} />
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-[240px]">
@@ -18,9 +18,7 @@ export default function Filters({ onFilter }: { onFilter?: Function }): ReactNod
             <p className="text-small font-bold text-foreground" {...titleProps}>
               Filters
             </p>
-            <div className="mt-2 flex flex-col gap-2 w-full">
-                    
-            </div>
+            <div className="mt-2 flex flex-col gap-2 w-full"></div>
           </div>
         )}
       </PopoverContent>
--- src/components/Modals/Login.tsx
+++ src/components/Modals/Login.tsx
@@ -1,10 +1,27 @@
-import { Input, Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, Checkbox } from "@nextui-org/react";
+"use client";
 import { type ReactNode, type KeyboardEvent, useState, useRef, useEffect } from "react";
 import { type SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ResizableDiv } from "@/components";
 import { toast } from "sonner";
+import {
+  Input,
+  Modal,
+  Button,
+  Checkbox,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  ModalContent,
+  useDisclosure,
+} from "@nextui-org/react";
 
 export default function LoginModal(): ReactNode {
-  return <div>ComponentName</div>;
+  const { isOpen, onClose } = useDisclosure();
+
+  return (
+    <Modal hideCloseButton backdrop="blur" size="md" placement="auto" isOpen={isOpen} onOpenChange={onClose}>
+      <ModalContent>{(onClose) => <></>}</ModalContent>
+    </Modal>
+  );
 }
