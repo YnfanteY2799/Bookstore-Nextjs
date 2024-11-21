58965524ff6fb8750bc111fd962047f86a82600b
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2024-11-21 19:06:29 -0400

    +adds

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,7 +1,7 @@
 import { AuthBasedUser, LangSwitcher, AuthModals, ThemeSwitcher } from "@/components";
 import Link from "next/link";
 
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 
 export default function Navbar(): ReactNode {
   return (
