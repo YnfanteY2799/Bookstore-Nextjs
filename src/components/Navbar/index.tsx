01ab7316d7825255515b6b61280a703aaa1809ab
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2024-11-21 15:26:05 -0400

    +adds

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,3 +1,4 @@
+import { ThemeSwitcher } from "@/components";
 import Link from "next/link";
 
 import { ReactNode } from "react";
@@ -6,18 +7,12 @@ export default function Navbar(): ReactNode {
   return (
     <header className="border-b">
       <nav className="container mx-auto px-4 py-4 flex justify-between items-center">
-        
-        <Link href="/" className="text-2xl font-bold">
+        <Link href="/Home" className="text-2xl font-bold">
           Minimalist Books
         </Link>
 
-
-
         <div className="space-x-4">
-          <Link href="/" className="text-sm hover:underline">
-            Home
-          </Link>
-          
+          <ThemeSwitcher />
         </div>
       </nav>
     </header>
--- src/components/Switchers/Theme.tsx
+++ src/components/Switchers/Theme.tsx
@@ -1,5 +1,25 @@
-import { ReactNode } from "react";
+"use client";
+import { useEffect, useState, type ReactNode } from "react";
+import { Button } from "@nextui-org/react";
+import { Sun, Moon } from "lucide-react";
+import { useTheme } from "next-themes";
+import { motion } from "framer-motion";
 
 export default function ThemeSwitcher(): ReactNode {
-  return <></>;
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+  const isDark = theme === "dark";
+
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+
+  const toggleTheme = () => setTheme((old) => (old === "light" ? "dark" : "light"));
+
+  return (
+    <Button onClick={toggleTheme} isIconOnly className="transition-colors duration-300" size="sm" variant="light">
+      <motion.div animate={{ rotate: isDark ? 0 : 360 }} transition={{ duration: 0.5, ease: "easeInOut" }}>
+        {isDark ? <Moon className="text-primary" size={20} /> : <Sun className="text-primary" size={20} />}
+      </motion.div>
+    </Button>
+  );
 }
