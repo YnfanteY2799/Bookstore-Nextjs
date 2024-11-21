349babbd38067b0a2a6cba12daca2cb274a924e8
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2024-11-21 18:04:40 -0400

    +adds

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,7 +7,8 @@ export * from "./store/appStore.ts";
 /* Store */
 
 /* Hooks */
-export { default as useLoginModal } from "./hooks/useLoginModal.ts";
+export { default as useRegisterModal } from "./hooks/store/useRegisterModal.ts";
+export { default as useLoginModal } from "./hooks/store/useLoginModal.ts";
 export { default as useIsoLayout } from "./hooks/useIsoLayout.ts";
 export { default as useMeasure } from "./hooks/useMeasure.ts";
 /* Hooks */
