7143edc6a2c3ef5ca4f961e9b3575b60683fb633
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2024-11-21 21:25:24 -0400

    +adds

--- src/components/Modals/Register/Modal.tsx
+++ src/components/Modals/Register/Modal.tsx
@@ -1,13 +1,13 @@
 "use client";
 import { Input, Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from "@nextui-org/react";
+import { type ReactNode, type KeyboardEvent, useRef, useState } from "react";
 import { RegisterModalFormSchema, TypeRegisterMFS } from "@/configs";
-import { type ReactNode, type KeyboardEvent, useRef } from "react";
 import { useLoginModal, useRegisterModal } from "@/utils/client";
 import { type SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useTranslations } from "next-intl";
 import { ResizableDiv } from "@/components";
-import { X } from "@phosphor-icons/react";
+import { Eye, EyeClosed, X } from "@phosphor-icons/react";
 import { RegisterService } from "@/api";
 import { toast } from "sonner";
 
@@ -21,19 +21,26 @@ export default function RegisterModal(): ReactNode {
   // Ref's
   const formRef = useRef<HTMLFormElement>(null);
 
+  // Simple State
+  const [showPw, setShowPw] = useState(false);
+
   // RHF
   const { handleSubmit, reset, formState, register } = useForm<TypeRegisterMFS>({
-    defaultValues: { email: "", username: "", password: "" },
+    defaultValues: { email: "", username: "", password: "", confirm_password: "" },
     resolver: zodResolver(RegisterModalFormSchema),
   });
 
   // Destructuring Error
-  const { email, password, username } = formState.errors;
+  const { email, password, username, confirm_password } = formState.errors;
 
   // Functions
   function handleSyntheticSubmit(): void {
     const curr = formRef.current;
     if (curr) curr.dispatchEvent(new Event("submit", { bubbles: true, cancelable: true }));
+  }
+
+  function showP() {
+    setShowPw((old) => !old);
   }
 
   function enterSubmit({ key }: KeyboardEvent): void {
@@ -75,11 +82,7 @@ export default function RegisterModal(): ReactNode {
             </ModalHeader>
             <ModalBody className="flex flex-col gap-0" onKeyDown={enterSubmit}>
               <ResizableDiv>
-                <form
-                  ref={formRef}
-                  onSubmit={handleSubmit(onSubmit)}
-                  className="flex flex-col gap-[10px] transition-height py-[2px]"
-                >
+                <form ref={formRef} onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-[6px] py-[1px]">
                   <Input
                     size="lg"
                     radius="md"
@@ -107,14 +110,28 @@ export default function RegisterModal(): ReactNode {
                   <Input
                     size="lg"
                     radius="md"
-                    type="text"
+                    maxLength={254}
+                    variant="bordered"
+                    isInvalid={!!password}
+                    labelPlacement="outside"
+                    {...register("password")}
+                    type={showPw ? "text" : "password"}
+                    label={commons("Form_Labels.password")}
+                    endContent={<ShowPBtn showP={showP} isActive={showPw} />}
+                    errorMessage={password ? commons(`Errors.${password.message}`) : ""}
+                  />
+                  <Input
+                    size="lg"
+                    radius="md"
                     maxLength={254}
                     variant="bordered"
                     labelPlacement="outside"
-                    isInvalid={!!password}
-                    {...register("password")}
-                    label={commons("Form_Labels.password")}
-                    errorMessage={password ? commons(`Errors.${password.message}`) : ""}
+                    isInvalid={!!confirm_password}
+                    {...register("confirm_password")}
+                    type={showPw ? "text" : "password"}
+                    label={commons("Form_Labels.confirm_password")}
+                    endContent={<ShowPBtn showP={showP} isActive={showPw} />}
+                    errorMessage={confirm_password ? commons(`Errors.${confirm_password.message}`) : ""}
                   />
                 </form>
               </ResizableDiv>
@@ -148,3 +165,9 @@ export default function RegisterModal(): ReactNode {
     </Modal>
   );
 }
+
+const ShowPBtn = ({ showP, isActive }: { showP: Function; isActive: boolean }): ReactNode => (
+  <Button size="sm" variant="light" isIconOnly color="warning" onPress={showP as any}>
+    {isActive ? <Eye size={18} /> : <EyeClosed size={18} />}
+  </Button>
+);
--- src/configs/schemas/Auth.ts
+++ src/configs/schemas/Auth.ts
@@ -11,15 +11,26 @@ export const RecoverFormSchema = z.object({ email: z.string().min(1, "emerror").
 
 export type TRecoverFS = z.infer<typeof RecoverFormSchema>;
 
-export const RegisterModalFormSchema = z.object({
-  username: z.string().min(8, { message: "unerror" }),
-  email: z.string().email("emerror").min(1, "emerror"),
-  password: z
-    .string()
-    .min(8, { message: "pwminerr" })
-    .regex(/[a-zA-Z]/, { message: "pwmoneletternerr" })
-    .regex(/[0-9]/, { message: "pwmonenumbernerr" })
-    .regex(/[^a-zA-Z0-9]/, { message: "pwmonespecnerr" }),
-});
+export const RegisterModalFormSchema = z
+  .object({
+    username: z.string().min(8, { message: "unerror" }),
+    email: z.string().email("emerror").min(1, "emerror"),
+    password: z
+      .string()
+      .min(8, { message: "pwminerr" })
+      .regex(/[a-zA-Z]/, { message: "pwmoneletternerr" })
+      .regex(/[0-9]/, { message: "pwmonenumbernerr" })
+      .regex(/[^a-zA-Z0-9]/, { message: "pwmonespecnerr" }),
+    confirm_password: z
+      .string()
+      .min(8, { message: "pwminerr" })
+      .regex(/[a-zA-Z]/, { message: "pwmoneletternerr" })
+      .regex(/[0-9]/, { message: "pwmonenumbernerr" })
+      .regex(/[^a-zA-Z0-9]/, { message: "pwmonespecnerr" }),
+  })
+  .refine(({ confirm_password, password }) => confirm_password === password, {
+    message: "errpwnm",
+    path: ["password", "confirm_password"],
+  });
 
 export type TypeRegisterMFS = z.infer<typeof RegisterModalFormSchema>;
